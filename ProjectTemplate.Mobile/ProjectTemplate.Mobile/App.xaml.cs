ee6568a31d1fd6b20d8f9fdb3dd64991e20c1e93
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2020-12-17 13:28:13 +0000

    Add project files.

---
+++ ProjectTemplate.Mobile/ProjectTemplate.Mobile/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ProjectTemplate.Mobile.Services;
+using ProjectTemplate.Mobile.ViewModels.Base;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectTemplate.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            if (Device.RuntimePlatform == Device.UWP)
+                InitNavigation();
+        }
+
+        private Task InitNavigation()
+        {
+            INavigationService navigationService = ViewModelLocator.Resolve<INavigationService>();
+            return navigationService.InitializeAsync();
+        }
+        protected override void OnStart()
+        {
+            if (Device.RuntimePlatform != Device.UWP)
+                InitNavigation();
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ProjectTemplate.Mobile/ProjectTemplate.Mobile/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProjectTemplate.Mobile.ViewModels.Base
+{
+    public class BaseViewModel : ObservableObject
+    {
+        public Dictionary<string, ICommand> Commands { get; protected set; }
+
+        public virtual async Task OnAppearingAsync() { }
+        public virtual async Task OnDisappearingAsync() { }
+
+        public BaseViewModel()
+        {
+            Commands = new Dictionary<string, ICommand>();
+        }
+
+        
+        public virtual Task InitializeAsync(object navigationData)
+        {
+            return Task.FromResult(false);
+        }
+
+        bool isBusy;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                if (SetProperty(ref isBusy, value))
+                    IsNotBusy = !isBusy;
+            }
+        }
+
+        bool isNotBusy = true;
+        public bool IsNotBusy
+        {
+            get => isNotBusy;
+            set
+            {
+                if (SetProperty(ref isNotBusy, value))
+                    IsBusy = !isNotBusy;
+            }
+        }
+
+
+    }
+}
