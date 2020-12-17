a32ef86c399e1b26a73c5bd73f15515773c7e531
Ryan Gaudion <rgaudion@gmail.com>
2020-12-17 13:28:13 +0000

    Add project files.

---
+++ ProjectTemplate.Mobile/ProjectTemplate.Mobile/Services/INavigationService.cs
@@ -0,0 +1,18 @@
+﻿using ProjectTemplate.Mobile.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemplate.Mobile.Services
+{
+    public interface INavigationService
+    {
+        BaseViewModel PreviousPageViewModel { get; }
+        Task InitializeAsync();
+        Task NavigateToAsync<TViewModel>() where TViewModel : BaseViewModel;
+        Task NavigateToAsync<TViewModel>(object parameter) where TViewModel : BaseViewModel;
+        Task RemoveLastFromBackStackAsync();
+        Task RemoveBackStackAsync();
+    }
+}
---
+++ ProjectTemplate.Mobile/ProjectTemplate.Mobile/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ProjectTemplate.Mobile.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTemplate.Mobile.ViewModels
+{
+    class HomePageViewModel : BaseViewModel
+    {
+    }
+}
