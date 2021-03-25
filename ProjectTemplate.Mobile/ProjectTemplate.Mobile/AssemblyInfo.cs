dbce0fc663b6788d807ff99eceb5dbc67d5ea358
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2021-03-25 19:00:50 +0000

    Font Awesome + Update Packages

--- ProjectTemplate.Mobile/ProjectTemplate.Mobile/AssemblyInfo.cs
+++ ProjectTemplate.Mobile/ProjectTemplate.Mobile/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FontAwesome5BrandsReg400.otf", Alias = "FABrands")]
+[assembly: ExportFont("FontAwesome5FreeReg400.otf", Alias = "FAReg")]
+[assembly: ExportFont("FontAwesome5FreeSolid900.otf", Alias = "FASolid")]
